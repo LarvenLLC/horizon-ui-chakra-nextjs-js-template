d0ff5892d7fe452c382345b965e1d722b9950585
BossBele <adambeleko@gmail.com>
2023-04-24 23:07:29 +0300

    remove typescript in code: src/components/scrollbar/Scrollbar.js

--- src/components/scrollbar/Scrollbar.js
+++ src/components/scrollbar/Scrollbar.js
@@ -12,20 +12,20 @@ export const renderTrack = ({ ...props }) => {
 		top: 2,
 		borderRadius: 3,
 		right: 0
-	} as const;
+	};
 	return <div style={ trackStyle } {...props} />;
 };
 export const renderThumb = ({  ...props }) => {
 	const thumbStyle = {
 		borderRadius: 15,
 		background: 'rgba(222, 222, 222, .1)'
-	} as const
+	}
 	return <div style={ thumbStyle } {...props} />;
 };
 export const renderView = ( ) => {
 	const viewStyle = {
 		marginBottom: -22
-	}as const;
+	};
 	return (
 		<Box height="100%" me={{ base: '0px !important', lg: '-16px !important' }} style={ viewStyle }   />
 	);
