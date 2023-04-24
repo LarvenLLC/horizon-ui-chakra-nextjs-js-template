34eec6f868614369e18f7b7032b7eb36d8d394f4
BossBele <adambeleko@gmail.com>
2023-04-24 23:07:29 +0300

    remove typescript in code: src/components/charts/LineChart.js

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -1,14 +1,11 @@
 import dynamic from 'next/dist/shared/lib/dynamic'
-import { ChartProps } from './LineAreaChart'
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
-
-interface LineChartProps extends ChartProps {}
 
 export default function LineChart ({
   chartOptions,
   chartData
-}: LineChartProps) {
+}) {
   return (
     <Chart
       options={chartOptions}
