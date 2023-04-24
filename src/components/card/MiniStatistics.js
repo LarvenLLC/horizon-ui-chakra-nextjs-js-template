dcdca5386513673c7dfc1b93d5ccd1fdeb4c5863
BossBele <adambeleko@gmail.com>
2023-04-24 23:07:29 +0300

    remove typescript in code: src/components/card/MiniStatistics.js

--- src/components/card/MiniStatistics.js
+++ src/components/card/MiniStatistics.js
@@ -3,13 +3,7 @@ import { Flex, Stat, StatLabel, StatNumber, useColorModeValue, Text } from '@cha
 // Custom components
 import Card from 'components/card/Card';
 
-export default function Default(props: {
-	startContent?: JSX.Element;
-	endContent?: JSX.Element;
-	name: string;
-	growth?: string | number;
-	value: string | number;
-}) {
+export default function Default(props) {
 	const { startContent, endContent, name, growth, value } = props;
 	const textColor = useColorModeValue('secondaryGray.900', 'white');
 	const textColorSecondary = 'secondaryGray.600';
